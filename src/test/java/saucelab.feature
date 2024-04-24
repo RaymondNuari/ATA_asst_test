95240ea54d31019b1728914e33724a4909a3fa9f
raymond.nuari <raymond.nuari@gojek.com>
2024-04-24 20:59:41 +0700

    mission complete

--- src/test/java/saucelab.feature
+++ src/test/java/saucelab.feature
@@ -10,3 +10,22 @@ Feature: SauceLabTest
     When user login with valid credential
     And user sorting the price high to low
     Then user see the price sorted from high to low
+
+  Scenario: verify user successful purchase for 2 items which has $15.99 price.
+    Given user open the saucelab login page
+    When user login with valid credential
+    And user purchase 2 item which has "$15.99" price
+    And user fill the buyer details
+    Then user successful complete purchase items
+
+  Scenario: verify user check the Log checkout summary.
+    Given user open the saucelab login page
+    When user login with valid credential
+    And user purchase 2 item which has "$15.99" price
+    And user fill the buyer details
+    Then user see correct details of checkout summary
+
+  Scenario: Login with locked_out_user account
+    Given user open the saucelab login page
+    When user login with "locked_out_user" username and "secret_sauce" password
+    Then user should see error message indicating the user is locked out
--- src/test/java/steps/SauceStepDef.java
+++ src/test/java/steps/SauceStepDef.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 public class SauceStepDef {
+
   private WebDriver driver;
 
   @Before
@@ -50,14 +51,15 @@ public class SauceStepDef {
   @Then("user see saucelab homepage")
   public void userSeeSaucelabHomepage() {
     assertTrue("Inventory page title should be displayed after login",
-      driver.findElement(By.className("title")).getText().contains("Products"));
+        driver.findElement(By.className("title")).getText().contains("Products"));
   }
 
   @And("user sorting the price high to low")
   public void userSortingThePriceHighToLow() {
     WebElement sortDropdown = driver.findElement(By.className("product_sort_container"));
     sortDropdown.click();
-    WebElement priceHighToLowOption = driver.findElement(By.xpath("//option[text()='Price (high to low)']"));
+    WebElement priceHighToLowOption = driver.findElement(
+        By.xpath("//option[text()='Price (high to low)']"));
     priceHighToLowOption.click();
 
   }
@@ -77,4 +79,69 @@ public class SauceStepDef {
           "Prices are not sorted from high to low");
     }
   }
+
+  @And("user purchase {int} item which has {string} price")
+  public void userPurchaseItemWhichHasPrice(int amount, String price) {
+    List<WebElement> items = driver.findElements(By.className("inventory_item_price"));
+    int itemCount = 0;
+    for (WebElement item : items) {
+      if (item.getText().contains(price)) {
+        WebElement addToCartButton = item.findElement(By.xpath("../../..//button"));
+        addToCartButton.click();
+        itemCount++;
+        if (itemCount == amount) {
+          break;
+        }
+      }
+    }
+    WebElement cartItemCount = driver.findElement(By.className("shopping_cart_badge"));
+    Assert.assertEquals(cartItemCount.getText(), "2", "Items not added to cart");
+
+    driver.findElement(By.className("shopping_cart_link")).click();
+    driver.findElement(By.id("checkout")).click();
+  }
+
+  @And("user fill the buyer details")
+  public void userFillTheBuyerDetails() {
+    driver.findElement(By.id("first-name")).sendKeys("Raymond");
+    driver.findElement(By.id("last-name")).sendKeys("Nuari");
+    driver.findElement(By.id("postal-code")).sendKeys("10440");
+    driver.findElement(By.id("continue")).click();
+  }
+
+  @Then("user successful complete purchase items")
+  public void userSuccessfulCompletePurchaseItems() {
+    WebElement checkoutTitle = driver.findElement(By.className("title"));
+    Assert.assertTrue(checkoutTitle.getText().contains("Checkout: Overview"), "Checkout failed");
+    driver.findElement(By.id("finish")).click();
+    Assert.assertTrue(driver.findElement(By.className("complete-header")).getText().contains("Thank you for your order!"), "Checkout failed");
+    driver.findElement(By.id("back-to-products")).click();
+    Assert.assertTrue(driver.findElement(By.className("title")).getText().contains("Products"));
+  }
+
+  @Then("user see correct details of checkout summary")
+  public void userSeeCorrectDetailsOfCheckoutSummary() {
+    WebElement checkoutTitle = driver.findElement(By.className("title"));
+    Assert.assertTrue(checkoutTitle.getText().contains("Checkout: Overview"), "Checkout failed");
+    List<WebElement> items = driver.findElements(By.className("inventory_item_price"));
+    for (WebElement item : items) {
+      Assert.assertTrue(item.getText().contains("15.99"), "item price wrong");
+    }
+  }
+
+  @When("user login with {string} username and {string} password")
+  public void userLoginWithUsernameAndPassword(String username, String password) {
+    driver.findElement(By.id("user-name")).sendKeys(username);
+    driver.findElement(By.id("password")).sendKeys(password);
+    driver.findElement(By.id("login-button")).click();
+  }
+
+  @Then("user should see error message indicating the user is locked out")
+  public void userShouldSeeErrorMessageIndicatingTheUserIsLockedOut() {
+    WebElement errorMessage = driver.findElement(By.cssSelector(".error-message-container"));
+    Assert.assertTrue(errorMessage.isDisplayed(), "Error message is not displayed");
+    Assert.assertTrue(errorMessage.getText().contains("Epic sadface: Sorry, this user has been locked out."), "Incorrect error message");
+  }
+
 }
+
